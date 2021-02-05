456e6de7475a631c9c1da20fd7b4b8ac75d05ff6
nick <nlomba1@gmail.com>
2021-02-05 17:18:09 -0500

    changedLink

--- src/GlobalFuncs.js
+++ src/GlobalFuncs.js
@@ -1,6 +1,6 @@
 // get the notes/folders from state via context
 
-export const BASE_URL = "https://guarded-everglades-44774.herokuapp.com";
+export const BASE_URL = "https://noteful-server-master.herokuapp.com";
 
 export const findFolder = (folders = [], folderId) =>
   folders.find((folder) => folder.id === folderId);
