9a98f876ac581ffc7411a6dd5b642eb87e7405f9
nick <nlomba1@gmail.com>
2021-02-05 16:44:25 -0500

    vercel

--- src/GlobalFuncs.js
+++ src/GlobalFuncs.js
@@ -1,11 +1,15 @@
 // get the notes/folders from state via context
 
-export const BASE_URL = 'http://localhost:9090'
+export const BASE_URL = "https://guarded-everglades-44774.herokuapp.com";
 
-export const findFolder = (folders=[], folderId) =>  folders.find(folder => folder.id === folderId)
+export const findFolder = (folders = [], folderId) =>
+  folders.find((folder) => folder.id === folderId);
 
-export const findNote = (notes=[], noteId) =>  notes.find(note => note.id === noteId)
+export const findNote = (notes = [], noteId) =>
+  notes.find((note) => note.id === noteId);
 
-export const getNotes = (notes=[], folderId) => (!folderId) ? notes : notes.filter(note => note.folderId === folderId) 
+export const getNotes = (notes = [], folderId) =>
+  !folderId ? notes : notes.filter((note) => note.folderId === folderId);
 
-export const getNoteCount = (notes=[], folderId) => notes.filter(note => note.folderId === folderId).length
+export const getNoteCount = (notes = [], folderId) =>
+  notes.filter((note) => note.folderId === folderId).length;
