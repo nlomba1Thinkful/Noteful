58e113ce63068ec0e77f578541efdf5ab58d3688
nick <nlomba1@gmail.com>
2021-02-08 12:35:54 -0500

    change

--- src/folder/FolderContents.js
+++ src/folder/FolderContents.js
@@ -1,69 +1,83 @@
-import React from 'react'
-import { format } from 'date-fns'
-import { Link, Redirect } from 'react-router-dom'
-import APIContext from '../APIContext'
-import { getNotes, BASE_URL } from '../GlobalFuncs'
-import PropTypes from 'prop-types'
+import React from "react";
+import { format } from "date-fns";
+import { Link, Redirect } from "react-router-dom";
+import APIContext from "../APIContext";
+import { getNotes, BASE_URL } from "../GlobalFuncs";
+import PropTypes from "prop-types";
 
 export default class FolderContents extends React.Component {
-    //pass the params
-    static defaultProps = {
-        match: {
-            params: {}
-        },
-        onDeleteNote: () => {}
-    }
-    //pass the folders and notes
-    static contextType = APIContext
+  //pass the params
+  static defaultProps = {
+    match: {
+      params: {},
+    },
+    onDeleteNote: () => {},
+  };
+  //pass the folders and notes
+  static contextType = APIContext;
 
-    render() { 
-        const { notes=[] } = this.context
-        const { folderId } = this.props.match.params
-        const handleClickDelete = e => {
-            e.preventDefault()
-            fetch(`${BASE_URL}/notes/${e.target.name}`, {
-              method: 'DELETE',
-              headers: {
-                'content-type': 'application/json'
-              },
-            })
-              .then(res => {
-                if (!res.ok)
-                  return res.json().then(e => Promise.reject(e))
-                return res.json()
-              })
-              .then(() => {
-                this.context.deleteNote(e.target.name)
-              })
-              .catch(error => {
-                console.error({ error })
-              })
-        }
-        const displayNotes = () => {
-            const notesToShow = getNotes(notes, folderId)
-            return notesToShow.map(note => {
-                return (
-                    <li key={note.id} className='note'>
-                        {/* add link to note */}
-                        <h2><Link to={`/note/${note.id}`}>{note.name}</Link></h2>
-                        <p>Date modified: {format(new Date(note.modified), 'MM/dd/yyyy')}</p>
-                        <button type="button" name={note.id} onClick={handleClickDelete}>Delete Note</button>
-                    </li>
-                )
-            })
-        }
-        if (getNotes(notes, folderId) !== ' ') {
+  render() {
+    const { notes = [] } = this.context;
+    const { folderId } = this.props.match.params;
+    const handleClickDelete = (e) => {
+      e.preventDefault();
+      fetch(`${BASE_URL}/notes/${e.target.name}`, {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+        .then((res) => {
+          if (!res.ok) return res.json().then((e) => Promise.reject(e));
+          return res.json();
+        })
+        .then(() => {
+          this.context.deleteNote(e.target.name);
+        })
+        .catch((error) => {
+          console.error({ error });
+        });
+    };
+    const displayNotes = () => {
+      const notesToShow = getNotes(notes, folderId);
+      return notesToShow.map((note) => {
         return (
-            <ul>
-                <li><button type="button" className="add-note-btn" onClick={e => this.props.history.push('/new-note')}>Add Note</button></li>
-                {displayNotes()}
-            </ul>
-        )}
-        return (<Redirect to={'/'}/>)
+          <li key={note.id} className="note">
+            {/* add link to note */}
+            <h2>
+              <Link to={`/note/${note.id}`}>{note.title}</Link>
+            </h2>
+            <p>
+              Date modified: {format(new Date(note.modified), "MM/dd/yyyy")}
+            </p>
+            <button type="button" name={note.id} onClick={handleClickDelete}>
+              Delete Note
+            </button>
+          </li>
+        );
+      });
+    };
+    if (getNotes(notes, folderId) !== " ") {
+      return (
+        <ul>
+          <li>
+            <button
+              type="button"
+              className="add-note-btn"
+              onClick={(e) => this.props.history.push("/new-note")}
+            >
+              Add Note
+            </button>
+          </li>
+          {displayNotes()}
+        </ul>
+      );
     }
-} 
+    return <Redirect to={"/"} />;
+  }
+}
 
 FolderContents.propTypes = {
   match: PropTypes.object.isRequired,
   onDeleteNote: PropTypes.func,
-}
+};
--- src/note/Note.js
+++ src/note/Note.js
@@ -1,69 +1,76 @@
-import React from 'react'
-import { Link, Redirect } from 'react-router-dom'
-import { format } from 'date-fns'
-import PropTypes from 'prop-types'
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import { format } from "date-fns";
+import PropTypes from "prop-types";
 
-import { BASE_URL, findNote } from '../GlobalFuncs'
+import { BASE_URL, findNote } from "../GlobalFuncs";
 
-import APIContext from '../APIContext'
+import APIContext from "../APIContext";
 
 export default class Note extends React.Component {
-    //pass the params
-    static defaultProps = {
-        match: {
-            params: {}
-        },
-        history: {
-            push: ()=>{}
-        },
-        onDeleteNote: () => {}
-    }
-    //pass the folders and notes
-    static contextType = APIContext
+  //pass the params
+  static defaultProps = {
+    match: {
+      params: {},
+    },
+    history: {
+      push: () => {},
+    },
+    onDeleteNote: () => {},
+  };
+  //pass the folders and notes
+  static contextType = APIContext;
 
-    render() {
-        const { notes=[] } = this.context
-        const { noteId } = this.props.match.params
-        const handleClickDelete = e => {
-            e.preventDefault()
-        
-            fetch(`${BASE_URL}/notes/${noteId}`, {
-              method: 'DELETE',
-              headers: {
-                'content-type': 'application/json'
-              },
-            })
-              .then(res => {
-                if (!res.ok)
-                  return res.json().then(e => Promise.reject(e))
-                return res.json()
-              })
-              .then(() => {
-                this.context.deleteNote(noteId)
-              })
-              .catch(error => {
-                console.error({ error })
-              })
-        }
-        
-        const currentNote = findNote(notes,noteId)
-        if (currentNote) {
-        return (
-            <section>
-                <div className='note'>
-                    <h2><Link to={`/note/${currentNote.id}`}>{currentNote.name}</Link></h2>
-                    <p>Date modified: {format(new Date(currentNote.modified), 'MM/dd/yyyy')}</p>
-                    <button type="button"onClick={handleClickDelete}>Delete Note</button>
-                </div>
-                <textarea cols='60' rows='20' value={currentNote.content}></textarea>
-            </section>
-        )}
-        return (<Redirect to={'/'}/>)
+  render() {
+    const { notes = [] } = this.context;
+    const { noteId } = this.props.match.params;
+    const handleClickDelete = (e) => {
+      e.preventDefault();
+
+      fetch(`${BASE_URL}/notes/${noteId}`, {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+        .then((res) => {
+          if (!res.ok) return res.json().then((e) => Promise.reject(e));
+          return res.json();
+        })
+        .then(() => {
+          this.context.deleteNote(noteId);
+        })
+        .catch((error) => {
+          console.error({ error });
+        });
+    };
+
+    const currentNote = findNote(notes, noteId);
+    if (currentNote) {
+      return (
+        <section>
+          <div className="note">
+            <h2>
+              <Link to={`/note/${currentNote.id}`}>{currentNote.title}</Link>
+            </h2>
+            <p>
+              Date modified:{" "}
+              {format(new Date(currentNote.modified), "MM/dd/yyyy")}
+            </p>
+            <button type="button" onClick={handleClickDelete}>
+              Delete Note
+            </button>
+          </div>
+          <textarea cols="60" rows="20" value={currentNote.content}></textarea>
+        </section>
+      );
     }
+    return <Redirect to={"/"} />;
+  }
 }
 
 Note.propTypes = {
   match: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired,
-  onDeleteNote: PropTypes.func
-}
+  onDeleteNote: PropTypes.func,
+};
