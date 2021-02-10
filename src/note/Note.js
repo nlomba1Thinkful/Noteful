eb9802cea0ab7b8e3f3ff5076b258696c951aefc
nick <nlomba1@gmail.com>
2021-02-10 11:55:24 -0500

    removed warning causing error

--- src/note/Note.js
+++ src/note/Note.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { format } from "date-fns";
 import PropTypes from "prop-types";
 
